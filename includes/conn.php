be2b6d54660138389c9113d59bf1053155a1fd89
Xuan-Truong <truong.xuan99419@gmail.com>
2021-11-02 17:42:54 +0700

    sqldone

--- includes/conn.php
+++ includes/conn.php
@@ -1,28 +1,28 @@
 <?php
 
-Class Database{
- 
+class Database
+{
+
 	private $server = "mysql5033.site4now.net";
 	private $username = "a7bea1_xtruong";
 	private $password = "99419Oggy";
-	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+	private $database  = "db_a7bea1_xtruong";
 	protected $conn;
- 	
-	public function open(){
- 		try{
- 			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
- 			return $this->conn;
- 		}
- 		catch (PDOException $e){
- 			echo "There is some problem in connection: " . $e->getMessage();
- 		}
- 
-    }
- 
-	public function close(){
-   		$this->conn = null;
- 	}
- 
+
+	public function open()
+	{
+		try {
+			$this->conn = new PDO($this->server, $this->username, $this->password, $this->database);
+			return $this->conn;
+		} catch (PDOException $e) {
+			echo "There is some problem in connection: " . $e->getMessage();
+		}
+	}
+
+	public function close()
+	{
+		$this->conn = null;
+	}
 }
 
 $pdo = new Database();
