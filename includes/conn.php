0df59e28170d98e50cfd829389cc83caed9c3af9
Xuan-Truong <truong.xuan99419@gmail.com>
2021-11-02 17:53:09 +0700

    db

--- includes/conn.php
+++ includes/conn.php
@@ -6,13 +6,14 @@ class Database
 	private $server = "mysql5033.site4now.net";
 	private $username = "a7bea1_xtruong";
 	private $password = "99419Oggy";
-	private $database  = "db_a7bea1_xtruong";
+	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+
 	protected $conn;
 
 	public function open()
 	{
 		try {
-			$this->conn = new PDO($this->server, $this->username, $this->password, $this->database);
+			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
 			return $this->conn;
 		} catch (PDOException $e) {
 			echo "There is some problem in connection: " . $e->getMessage();
